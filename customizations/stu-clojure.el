8f4bcb903aec93424493fce2cdd173858795532d
Stuart King <stuartrexking@gmail.com>
2017-07-09 11:27:13 +1000

    Configure clj-refactor

--- customizations/stu-clojure.el
+++ customizations/stu-clojure.el
@@ -1,17 +1,25 @@
 (require 'use-package)
 
 (use-package clojure-mode)
-(use-package clj-refactor)
+
+(use-package clj-refactor
+  :ensure t
+  :diminish clj-refactor-mode
+  :after clojure-mode
+  :config
+  (yas-minor-mode 1)
+  (clj-refactor-mode 1))
+
 (use-package clojure-mode-extra-font-locking)
 (use-package cider
   :init
   (setq cider-cljs-lein-repl
-    "(do (require 'figwheel-sidecar.repl-api)
+	"(do (require 'figwheel-sidecar.repl-api)
        (figwheel-sidecar.repl-api/start-figwheel!)
        (figwheel-sidecar.repl-api/cljs-repl))"))
 
 (add-hook 'clojure-mode-hook #'rainbow-delimiters-mode)
 
 (defun clojure-mode-customizations-hook ())
-  
+
 (add-hook 'clojure-mode-hook 'clojure-mode-customizations-hook)
