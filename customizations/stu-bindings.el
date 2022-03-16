6f6843eed5c3830b7083b846014996b4c4848459
Stuart King <stuartrexking@gmail.com>
2022-03-17 09:59:53 +1000

    Changed navigation mappings

--- customizations/stu-bindings.el
+++ customizations/stu-bindings.el
@@ -4,16 +4,16 @@
   (yank-pop (- arg)))
 
 (global-set-key (kbd "M-Y") 'yank-pop-forwards)
-(global-set-key (kbd "C-c j") 'previous-buffer)
-(global-set-key (kbd "C-c k") 'next-buffer)
-
+(global-set-key (kbd "s-j") 'previous-buffer)
+(global-set-key (kbd "s-k") 'next-buffer)
 
 ;;toggle line comments with C-;
 (defun toggle-comment-on-line ()
   "comment or uncomment current line"
   (interactive)
   (comment-or-uncomment-region (line-beginning-position) (line-end-position)))
-(global-set-key (kbd "C-;") 'toggle-comment-on-line)
+
+(global-set-key (kbd "C-.") 'toggle-comment-on-line)
 
 ;; move text
 (use-package move-text
--- customizations/stu-navigation.el
+++ customizations/stu-navigation.el
@@ -1,6 +1,6 @@
-(global-set-key (kbd "C-.") 'other-window)
-(global-set-key (kbd "C-.") 'prev-window)
-
 (defun prev-window ()
   (interactive)
   (other-window -1))
+
+(global-set-key (kbd "C-;") 'other-window)
+(global-set-key (kbd "C-;") 'prev-window)
