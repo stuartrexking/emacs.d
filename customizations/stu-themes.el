1390e515f4707f3f0ab185785a3a715775687359
Stuart King <stuartrexking@gmail.com>
2022-03-18 09:23:10 +1000

    Install Kaolin themes.

--- customizations/stu-themes.el
+++ customizations/stu-themes.el
@@ -5,7 +5,15 @@
   :config
   (setq doom-themes-enable-bold t
         doom-themes-enable-italic t)
-  (load-theme 'doom-vibrant t))
+  ;; (load-theme 'doom-zenburn t)
+)
+
+;; Kaolin themes are also really nice
+;; https://github.com/ogdenwebb/emacs-kaolin-themes
+(use-package kaolin-themes
+  :config
+  (load-theme 'kaolin-aurora t)
+  (kaolin-treemacs-theme))
 
 (use-package doom-modeline
   :ensure t
