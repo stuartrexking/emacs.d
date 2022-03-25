3aa57f2878651d822e186916c44210e467474aca
Stuart King <stuartrexking@gmail.com>
2022-03-25 19:59:52 +1000

    Use doom-material theme

--- customizations/stu-themes.el
+++ customizations/stu-themes.el
@@ -5,15 +5,15 @@
   :config
   (setq doom-themes-enable-bold t
         doom-themes-enable-italic t)
-  ;; (load-theme 'doom-zenburn t)
+  (load-theme 'doom-material t)
 )
 
 ;; Kaolin themes are also really nice
 ;; https://github.com/ogdenwebb/emacs-kaolin-themes
-(use-package kaolin-themes
-  :config
-  (load-theme 'kaolin-aurora t)
-  (kaolin-treemacs-theme))
+;; (use-package kaolin-themes
+  ;; :config
+  ;; (load-theme 'kaolin-aurora t)
+  ;; (kaolin-treemacs-theme))
 
 (use-package doom-modeline
   :ensure t
